19b5e6fe1bcc59f9a4e40d7bb3422209a8a10eed
Richard Murray <rfm@EL-PF2RR967.yorku.yorku.ca>
2022-09-12 11:59:28 -0400

    posted lecture 2 code

---
+++ 02 - flow; dictionaries/workshop2.py
@@ -0,0 +1,23 @@
+# workshop2.py  Lecture 2 workshop problems
+
+# Add code below the following comments, to solve the stated problems.
+
+# 1. Use a for loop to create a list that contains the squares of the
+# integers 1 to 10.
+
+# 2. Repeat problem 1, using a while loop instead of a for loop.
+
+# 3. Repeat problem 1, using a list comprehension instead of a for loop.
+
+# 4. Rank the solutions in problems 1 to 3 from best to worst. Explain
+# your ranking.
+
+# 5. Use a loop to find the smallest prime number whose square is greater
+# than 1000.
+
+# 6. Create a list with 100 elements. Each element of the list is a tuple
+# with two elements. The first element of each tuple is a random number
+# from the interval [0,1). The second element of each tuple is a random
+# number drawn from the interval [0,1), with the constraint that the second
+# element is greater than the first element. (So actually the second element
+# is a sample from [x,1), where x is the first element.)
