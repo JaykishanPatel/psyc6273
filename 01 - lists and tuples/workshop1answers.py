a6e85741a1c72bf6bf1ff20e5d9ffa211057abee
Richard Murray <rfm@SB-W362Y-FJ1J3.yorku.yorku.ca>
2022-09-15 12:54:05 -0400

    posted answers

---
+++ 01 - lists and tuples/workshop1answers.py
@@ -0,0 +1,23 @@
+# workshop1answers.py Lecture 1 workshop problems, with solutions
+
+# Add code below the following comments, to solve the stated problems.
+
+# 1. Create a list x that contains the numbers 1 to 10.
+x = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+# 2. Set a variable y equal to the following sublists of x. Where possible,
+# write your code so that it works for any length of x, e.g., it still
+# works if x has 20 elements rather than 10.
+
+# 2a. the first element of x
+y = x[0]
+
+# 2b. the first five elements of x
+y = x[:5]
+
+# 2c. the last element of x
+y = x[-1]
+
+# 2d. the last five elements of x
+y = x[-5:]
+
