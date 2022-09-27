f0c3368571ffac5f93e8391ccf79de738c99aaf4
Richard Murray <rfm@yorku.ca>
2022-09-27 18:02:06 -0400

    posted lecture code

---
+++ 03 - functions; strings/text_files.py
@@ -0,0 +1,19 @@
+# text_files.py  Creating text files
+
+import random, time
+
+# open the file
+f = open('data.txt', 'a')
+# 'a' = if the file exists, append to it
+# 'w' = if the file exists, overwrite it
+# 'r' = read
+
+# write some lines to the file
+for i in range(100):
+    randnum = random.uniform(0, 1)
+    sampletime = time.time()
+    f.write(f'{i+1:3d}, {randnum:.2f}, {sampletime:.9f}\n')
+    
+# close the file
+f.close()
+
