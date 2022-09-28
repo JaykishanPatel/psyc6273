98d65f13297b6523f78165916ea291a9dfc5c092
Richard Murray <rfm@SB-G322-L8893.yorku.yorku.ca>
2022-09-28 14:39:48 -0400

    added chdir command

--- 03 - functions; strings/text_files.py
+++ 03 - functions; strings/text_files.py
@@ -1,6 +1,11 @@
 # text_files.py  Creating text files
 
-import random, time
+import os, random, time
+
+# on startup, PsychoPy's current working directory is in its own
+# application folder, so we'll cd to someplace else before creating
+# the data file
+os.chdir('/Users/rfm/Desktop')
 
 # open the file
 f = open('data.txt', 'a')
@@ -13,7 +18,6 @@ for i in range(100):
     randnum = random.uniform(0, 1)
     sampletime = time.time()
     f.write(f'{i+1:3d}, {randnum:.2f}, {sampletime:.9f}\n')
-    
+
 # close the file
 f.close()
-
